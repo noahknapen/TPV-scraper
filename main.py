8fdc194daa7466418688e316c0e01130e1d8d7f2
Noah Knapen <noah.knapen@gmail.com>
2025-11-20 20:17:00 +0100

    Added functionality for tennis

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ import datetime
 # TODO: Do not hardcode the number of fields
 # TODO: Make applicable for tennis courts (add flag that looks for 'padel' or 'tennis')
 
-def generate_url(day, month, year):
+def generate_url(tennis, day, month, year):
     # TODO: Add checks for proper arguments
-    return f"https://www.tennisenpadelvlaanderen.be/clubdashboard/reserveer-een-terrein?clubId=1956&planningDay={day}-{month}-{year}&terrainGroupId=9565&ownClub=true&clubCourts%5B0%5D=I&clubCourts%5B1%5D=O#hash_results"
+
+    if tennis:
+        return f"https://www.tennisenpadelvlaanderen.be/clubdashboard/reserveer-een-terrein?clubId=1956&planningDay={day}-{month}-{year}&terrainGroupId=3464&ownClub=true&clubCourts[0]=I&clubCourts[1]=O#hash_results"
+    else:
+        return f"https://www.tennisenpadelvlaanderen.be/clubdashboard/reserveer-een-terrein?clubId=1956&planningDay={day}-{month}-{year}&terrainGroupId=9565&ownClub=true&clubCourts%5B0%5D=I&clubCourts%5B1%5D=O#hash_results"
 
 def get_page(url):
     try:
@@ -24,30 +28,31 @@ def get_page(url):
 
     return text
 
-def remove_irrelevant_characters(parsed_html):
+def remove_irrelevant_characters(tennis, parsed_html):
     # Keywords: <hh:mm>, padel <1|2>, <vrij|bezet>
     # Remove all other characters
 
-    filtered_html = re.findall(r"[0-9]{2}:[0-9]{2}|padel [12]|vrij|bezet", parsed_html)
-    return filtered_html
+    if tennis:
+        return re.findall(r"[0-9]{2}:[0-9]{2}|terrein [0-9]{1,}|vrij|bezet|verhuurd", parsed_html)
+    else:
+        return re.findall(r"[0-9]{2}:[0-9]{2}|padel [0-9]{1,}|vrij|bezet|verhuurd", parsed_html)
 
 def get_occupied_hours(filtered_html):
     # TODO: add out-of-range checks
 
     hour = None
-    occupied_hours = {'field1': [], 'field2': []}
+    occupied_hours = {}
 
     for index in range(0, len(filtered_html)):
         if re.fullmatch(r'[0-9]{2}:[0-9]{2}', filtered_html[index]):
             hour = filtered_html[index]
-        if filtered_html[index] == 'bezet':
+        if filtered_html[index] == 'bezet' or filtered_html[index] == 'verhuurd':
             field = filtered_html[index-1]
-            if field == 'padel 1':
-                occupied_hours["field1"].append(hour)
-            elif field == 'padel 2':
-                occupied_hours["field2"].append(hour)
+
+            if field in occupied_hours:
+                occupied_hours[field].append(hour)
             else:
-                raise Exception("Where is the field specification?")
+                occupied_hours[field] = [hour]
         
     return occupied_hours
 
@@ -56,11 +61,13 @@ def get_fully_occupied_days_and_hours(occupied_days):
     fully_occupied = {}
 
     for day in occupied_days.keys():
-        field1_hours = occupied_days[day]['field1']
-        field2_hours = occupied_days[day]['field2']
+        fully_occupied[day] = []
+        for field in occupied_days[day].keys():
+            if fully_occupied[day] == []:
+                fully_occupied[day] = occupied_days[day][field]
+            else:
+                fully_occupied[day] = [hour for hour in fully_occupied[day] if hour in occupied_days[day][field]]
 
-        fully_occupied[day] = [hour for hour in field1_hours if hour in field2_hours]
-    
     return fully_occupied
 
 def get_nb_occurrences_fully_occupied(fully_occupied):
@@ -75,7 +82,8 @@ def get_nb_occurrences_fully_occupied(fully_occupied):
 def main():
     parser = argparse.ArgumentParser(description="Parse the HTML code of the supplied URL")
     parser.add_argument("-d", "--day", required=True, help="The start day to fetch the data from (dd-mm-yyyy)")
-    parser.add_argument('--b', action='store_true')
+    parser.add_argument('--b', action='store_true', help='If supplied, return the number of occurrences the fields are all fully booked')
+    parser.add_argument('--t', action='store_true', help='If supplied, fetch the results for tennis instead of padel')
     args = parser.parse_args()
 
     day = args.day.split(sep="-")
@@ -85,10 +93,10 @@ def main():
 
     while analyze_date <= today:
         print(f"Analyzing {analyze_date}")
-        url = generate_url(analyze_date.strftime("%d"), analyze_date.strftime("%m"), analyze_date.strftime("%Y"))
+        url = generate_url(args.t, analyze_date.strftime("%d"), analyze_date.strftime("%m"), analyze_date.strftime("%Y"))
 
         parsed_html = get_page(url)
-        filtered_html = remove_irrelevant_characters(parsed_html)
+        filtered_html = remove_irrelevant_characters(args.t, parsed_html)
         occupied_hours = get_occupied_hours(filtered_html)
         occupied_days[analyze_date.strftime("%d-%m-%Y")] = occupied_hours
         analyze_date += datetime.timedelta(days=1)
