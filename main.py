b4a3bc9bc7814dea3836ea99fcd9a653761ae753
Noah Knapen <noah.knapen@gmail.com>
2025-11-20 19:35:00 +0100

    Get the number of occurrences that all fields are booked at the same hour

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 import re
 import datetime
 
+# TODO: Do not hardcode the number of fields
+# TODO: Make applicable for tennis courts (add flag that looks for 'padel' or 'tennis')
+
 def generate_url(day, month, year):
     # TODO: Add checks for proper arguments
     return f"https://www.tennisenpadelvlaanderen.be/clubdashboard/reserveer-een-terrein?clubId=1956&planningDay={day}-{month}-{year}&terrainGroupId=9565&ownClub=true&clubCourts%5B0%5D=I&clubCourts%5B1%5D=O#hash_results"
@@ -48,9 +51,31 @@ def get_occupied_hours(filtered_html):
         
     return occupied_hours
 
+def get_fully_occupied_days_and_hours(occupied_days):
+
+    fully_occupied = {}
+
+    for day in occupied_days.keys():
+        field1_hours = occupied_days[day]['field1']
+        field2_hours = occupied_days[day]['field2']
+
+        fully_occupied[day] = [hour for hour in field1_hours if hour in field2_hours]
+    
+    return fully_occupied
+
+def get_nb_occurrences_fully_occupied(fully_occupied):
+
+    occurrences = 0
+
+    for hours in fully_occupied.values():
+        occurrences += len(hours)
+    
+    return occurrences
+
 def main():
     parser = argparse.ArgumentParser(description="Parse the HTML code of the supplied URL")
     parser.add_argument("-d", "--day", required=True, help="The start day to fetch the data from (dd-mm-yyyy)")
+    parser.add_argument('--b', action='store_true')
     args = parser.parse_args()
 
     day = args.day.split(sep="-")
@@ -59,6 +84,7 @@ def main():
     occupied_days = {}
 
     while analyze_date <= today:
+        print(f"Analyzing {analyze_date}")
         url = generate_url(analyze_date.strftime("%d"), analyze_date.strftime("%m"), analyze_date.strftime("%Y"))
 
         parsed_html = get_page(url)
@@ -67,7 +93,14 @@ def main():
         occupied_days[analyze_date.strftime("%d-%m-%Y")] = occupied_hours
         analyze_date += datetime.timedelta(days=1)
 
-    print(occupied_days)
+    if args.b:
+        fully_occupied_days_and_hours = get_fully_occupied_days_and_hours(occupied_days)
+        print(fully_occupied_days_and_hours)
+        fully_occupied_occurrences = get_nb_occurrences_fully_occupied(fully_occupied_days_and_hours)
+        print(fully_occupied_occurrences)
+    else:
+        print(occupied_days)
+
     return
 
 if __name__ == "__main__":
